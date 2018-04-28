0da2eca41793d352d786bdb941d19c50b49285f9
Pawel Gniewek <gniewko.pablo@gmail.com>
2018-04-27 20:06:35 -0700

    Core of the game-playing script

---
+++ play-enigma.py
@@ -0,0 +1,18 @@
+# OpenGym MagicSqaure-v0
+# -------------------
+#
+# author: Pawel Gniewek, 2018
+
+from keras import backend as K
+import tensorflow as tf
+
+
+
+
+if __name__ == "__main__":
+
+    PROBLEM = 'MagicSquare3x3P1-v0'
+    nn_model = 'model-1'
+
+
+
