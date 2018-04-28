ce6cdcc93b8c371563ba7ab5c68ba8416641df03
Pawel Gniewek <gniewko.pablo@gmail.com>
2018-04-27 20:53:35 -0700

    load keras model

--- play-enigma.py
+++ play-enigma.py
@@ -5,14 +5,12 @@
 
 from keras import backend as K
 import tensorflow as tf
-
-
-
+from keras.models import load_model
 
 if __name__ == "__main__":
 
     PROBLEM = 'MagicSquare3x3P1-v0'
-    nn_model = 'model-1'
-
-
+    model_dir = './model'
+    model_file = model_dir + "/" + PROBLEM + "-dqn.h5"
+    model = load_model(model_file)
 
