a9c1fd18e0a22674d7ccdffd0bcc4bc9e9605089
Pawel Gniewek <gniewko.pablo@gmail.com>
2018-04-27 18:07:05 -0700

    Length of the game extended to 10k

--- gym_magic/__init__.py
+++ gym_magic/__init__.py
@@ -6,19 +6,19 @@ logger = logging.getLogger(__name__)
 register(
     id='MagicSquare3x3P1-v0',
     entry_point='gym_magic.envs:MagicSquare3x3P1',
-    timestep_limit=5000,
+    timestep_limit=10000,
 )
 
 
 register(
     id='MagicSquare6x6P1-v0',
     entry_point='gym_magic.envs:MagicSquare6x6P1',
-    timestep_limit=5000,
+    timestep_limit=10000,
 )
 
 
 register(
     id='MagicSquare6x6P3-v0',
     entry_point='gym_magic.envs:MagicSquare6x6P3',
-    timestep_limit=5000,
+    timestep_limit=10000,
 )
