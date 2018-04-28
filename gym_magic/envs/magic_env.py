1209ff8f7e1ee9e6df8867ba41281ad6b0bc80c5
Pawel Gniewek <gniewko.pablo@gmail.com>
2018-04-27 17:04:32 -0700

    First working version

--- gym_magic/envs/magic_env.py
+++ gym_magic/envs/magic_env.py
@@ -16,16 +16,15 @@ from gym import spaces
 from gym.utils import seeding
 
 
-class MagicSquae(gym.Env):
+class MagicSquareEnv(gym.Env):
 
     def __init__(self, DIM_=3, POW_=1):
-        self.__version__ = "0.1.0"
-        print("MagicSqaure - Version {}".format(self.__version__))
-
+   
         # General variables defining the environment
         self.DIM = DIM_
         self.POW = POW_
         self.action_space = spaces.Discrete(2*self.DIM*self.DIM)
+        self.observation_space = np.ones(self.DIM*self.DIM)
         self.state = None
         self.is_square_solved=False
         self.curr_step = -1 
@@ -36,65 +35,99 @@ class MagicSquae(gym.Env):
         self.reset()
  
        # Just need to initialize the relevant attributes
-        self._configure()
 
 
     def step(self, action):
-        """
-        """
         if self.is_square_solved:
             raise RuntimeError("Episode is done")
 
         self.curr_step += 1
-        self._take_action(action)
+        new_state = self._take_action(action)
         reward = self._get_reward()
         info_ = {}
-        return self.state, reward, self.is_square_solved, info_
+        self.is_square_solved = reward == 2**(2*self.DIM+2)
+        if self.is_square_solved:
+            pass
+            # print out a magic square
+
+        return new_state, reward, self.is_square_solved, info_
 
     def _take_action(self, action):
-        return 
+        if action < self.DIM*self.DIM:
+            self.state[action] += 1
+        else:
+            if self.state[action - self.DIM*self.DIM] > 1:
+                self.state[action - self.DIM*self.DIM] -= 1
+                
+            
+        return self.state
 
     def _get_reward(self):
-        return 0.0
+        if 0 in self.state:
+            return 0.0
+        u_, c_ = np.unique(self.state,return_counts=True)
+        if c_.max() > 1:
+            return 0.0
+
+        pow_numbers = np.power(self.state,self.POW)
+        ms_ = pow_numbers.reshape( (self.DIM,self.DIM) )
+        row_sums =  np.sum(ms_,axis=1)
+        column_sums = np.sum(ms_,axis=0)
+        diagonal_sums = np.array( [np.trace(ms_), np.trace(np.flip(ms_,1)) ] )
+      
+        sums_ = np.append(np.append( row_sums, column_sums), diagonal_sums  )
+        reward = self._calc_reward_score(sums_)
+        #if reward > 2:
+        #    print reward, ms_, sums_
+        return reward
+
+
+    def _calc_reward_score(self, sums_):
+        uniqs_, counts_ = np.unique(sums_,return_counts=True)
+        reward = 2**( counts_.max() )
+        return reward
+
 
     def reset(self):
-        """
-        Reset the state of the environment and returns an initial observation.
-
-        Returns
-        -------
-        observation (object): the initial observation of the space.
-        """
+        #self.state = np.random.randint(1,self.DIM*self.DIM, size=self.DIM*self.DIM)
+        self.state = np.arange( 1, self.DIM*self.DIM+1, 1, dtype=int)
+        np.random.shuffle( self.state )
         return self.state
+
 
     def render(self, mode='human', close=False):
         return
-    
+
+
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
-    def _configure(self):
-        return 
 
-class MagicSquare3x3P1(MagicSquare):
+class MagicSquare3x3P1(MagicSquareEnv):
     """
     """
     def __init__(self):
+        self.__version__ = "0.1"
+        print("MagicSqaure3x3P1 - Version {}".format(self.__version__))
         super(MagicSquare3x3P1, self).__init__(DIM_=3,POW_=1)
 
 
-class MagicSquare3x3P1(MagicSquare):
+class MagicSquare6x6P1(MagicSquareEnv):
     """
     """
     def __init__(self):
-        super(MagicSquare3x3P1, self).__init__(DIM_=6,POW_=1)
+        self.__version__ = "0.1"
+        print("MagicSqaure6x6P1 - Version {}".format(self.__version__))
+        super(MagicSquare6x6P1, self).__init__(DIM_=6,POW_=1)
 
 
-class MagicSquare6x6P3(MagicSquare):
+class MagicSquare6x6P3(MagicSquareEnv):
     """
     """
     def __init__(self):
+        self.__version__ = "0.1"
+        print("MagicSqaure6x6P3 - Version {}".format(self.__version__))
         super(MagicSquare6x6P3, self).__init__(DIM_=6,POW_=3)
 
 
